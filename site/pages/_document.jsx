173d76d0b7e94aa97bf2757b9234d799aa449388
mrth2 <mr.th2.90@gmail.com>
2022-06-03 00:04:05 +0700

    add klarna script after body

--- site/pages/_document.jsx
+++ site/pages/_document.jsx
@@ -8,6 +8,11 @@ export default class DasDocument extends Document {
       <Html>
         <Head />
         <body id={pageProps.bodyId} className={pageProps.bodyClass}>
+          <script
+            async
+            src="https://na-library.klarnaservices.com/lib.js"
+            data-client-id="acfb1e1e-8879-5051-bc94-5c9a72e324fc"
+          />
           <Main />
           <NextScript />
         </body>
