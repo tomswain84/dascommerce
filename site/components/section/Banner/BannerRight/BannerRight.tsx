166d68c422ecce532b3aeca95f87933312e575aa
mrth2 <mr.th2.90@gmail.com>
2022-06-16 14:51:39 +0700

    fix banner right image

--- site/components/section/Banner/BannerRight/BannerRight.tsx
+++ site/components/section/Banner/BannerRight/BannerRight.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+import stripHTML from "@lib/strip-html"
 import type { VFC } from "react"
 import { BannerProps } from "../Banner"
 
@@ -8,7 +9,7 @@ const BannerRight: VFC<BannerProps> = ({ content, image }) => {
     <div className="container-boxed">
       <div className="row align-items-center">
         <div className="col-12 col-md-6 order-md-2">
-          <img className="img-fluid rounded mb-3 mb-md-0" src="../images/product-images/product-details/4-professional/img-n_key_rollover.png" alt="N-Key Rollover" />
+          <img className="img-fluid rounded mb-3 mb-md-0" src={image.src} alt={stripHTML(title)} />
         </div>
         <div className="col-12 col-md-6">
           <figure className="heading mb-0">
