d4788e6770265ed9e2ef9e714523644b40eea850
mrth2 <mr.th2.90@gmail.com>
2022-06-16 19:42:54 +0700

    auto redirect old category link to new links

--- site/pages/products/_middleware.ts
+++ site/pages/products/_middleware.ts
@@ -3,8 +3,13 @@ import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
   if (req.page.name === "/products/[path]") {
     const path = req.page.params?.path
-    if (path && path.includes('daskeyboard-')) {
-      return NextResponse.redirect(`/products/${path.replace('daskeyboard-', 'daskeyboard/')}`, 302);
+    if (path) {
+      if (path.startsWith('daskeyboard-')) {
+        return NextResponse.redirect(`/products/${path.replace('daskeyboard-', 'daskeyboard/')}`, 302);
+      }
+      else if (path.startsWith('category-')) {
+        return NextResponse.redirect(`/products/${path.replace('category-', 'category/')}${req.nextUrl.search}`, 302);
+      }
     }
   }
   return NextResponse.next()
