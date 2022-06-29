6dc3ccadee98b2eab0f6bd61a40448ef61c5bc00
mrth2 <mr.th2.90@gmail.com>
2022-06-29 10:03:37 +0700

    add google tag manager scripts

--- site/pages/_document.jsx
+++ site/pages/_document.jsx
@@ -1,13 +1,27 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-
 export default class DasDocument extends Document {
   render() {
     const pageProps = this.props?.__NEXT_DATA__?.props?.pageProps
 
     return (
       <Html>
-        <Head />
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5NHJTD6');`,
+            }}
+          />
+        </Head>
         <body id={pageProps.bodyId} className={pageProps.bodyClass}>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5NHJTD6"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           <script
             async
             src="https://na-library.klarnaservices.com/lib.js"
