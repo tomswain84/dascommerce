da849fe8ef32d0db969142a660898eba564cd219
mrth2 <mr.th2.90@gmail.com>
2022-07-14 10:37:09 +0700

    translate affiliate programs

--- site/pages/affiliates-program.tsx
+++ site/pages/affiliates-program.tsx
@@ -1,12 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 import ListItem from "@components/core/ListItem"
 import PageTitle from "@components/core/PageTitle"
+import useTrans, { getTrans } from "lang/useTrans"
+import { GetStaticPropsContext } from "next"
 import type { VFC } from "react"
 
-export async function getStaticProps() {
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+  const { say } = getTrans(locale)
   return {
     props: {
-      title: "AFFILIATE PROGRAM",
+      title: say('affiliates-program'),
       bodyId: 'affiliate-program',
       bodyClass: 'single-content',
     },
@@ -14,6 +17,7 @@ export async function getStaticProps() {
 }
 
 const AffiliatesProgram: VFC<{}> = () => {
+  const { say, $html } = useTrans()
   return (
     <>
       <PageTitle
@@ -25,30 +29,17 @@ const AffiliatesProgram: VFC<{}> = () => {
           <div className="container">
             <div className="row">
               <div id="contentContainer" className="col">
-                <h2>Become a Das Keyboard Affiliate</h2>
-                <p>If you are a technology enthusiast, social media influencer, or if you manage a website or blog with a tech-savvy audience, then sign up with Rakuten to join our affiliate program and start making money promoting Das Keyboard’s premium products today. With unmatched quality, a competitive price point, an extremely loyal customer base, and a very high average order value, it’s easy! With a base rate of 6%, and bonuses and bounties throughout the year, your audience and bank account will thank you.</p>
+                <h2>{say('affiliates-program-subtitle-1')}</h2>
+                <p dangerouslySetInnerHTML={$html('affiliates-program-content-1')} />
 
-                <h3>Terms</h3>
-                <ol>
-                  <li>
-                    Create a Rakuten Linkshare affiliate network account<br />
-                    <a href="https://signup.linkshare.com/publishers/registration/landing?mid=38366" target="_blank" title="Sign Up" rel="noreferrer">Sign up</a> and once you complete the signup process, it will automatically redirect you to the Das Keyboard registration page.
-                  </li>
-                  <li className="mt-3">
-                    Already a Rakuten Affiliate?<br />
-                    <a href="https://login.linkshare.com/sso/login?service=http%3A%2F%2Fcli.linksynergy.com%2Fcli%2Fpublisher%2Fprograms%2Fadvertiser_detail.php%3Foid%3D%5B461141%5D%26mid%3D%5B38366%5D%26controls%3D1%3A1%3A0%3A3%3A1%3A1" target="_blank" title="Sign Up" rel="noreferrer">Join our program!</a>
-                  </li>
-                  <li className="mt-3">
-                    Promote<br />
-                    Promote our high-performance mechanical keyboards to your audience through links or banners.
-                  </li>
-                  <li className="mt-3">
-                    Earn<br />
-                    Start earning 6% on all orders you refer to Das Keyboard.
-                  </li>
-                </ol>
+                <h3>{say('affiliates-program-subtitle-2')}</h3>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: $html('affiliates-program-content-2').__html.replace(/\<li\>/g, '<li class="mt-3">')
+                  }}
+                />
 
-                <h3>Details</h3>
+                <h3>{say('affiliates-program-subtitle-3')}</h3>
                 <ul className="list-unstyled">
                   <ListItem>Orders will lock in 30-days</ListItem>
                   <ListItem>The order value does not include shipping or tax</ListItem>
