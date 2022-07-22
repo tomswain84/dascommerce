f22b80a0e0d9d2eb23405cb7da8f2bfbd9eaea60
mrth2 <mr.th2.90@gmail.com>
2022-07-22 13:59:32 +0700

    translate 4C TKL

--- site/components/daskeyboard/4CTkl/4CTkl.tsx
+++ site/components/daskeyboard/4CTkl/4CTkl.tsx
@@ -8,13 +8,15 @@ import TechnicalSpecifications from "@components/section/TechnicalSpecifications
 import AppletDashboard from "@components/product/AppletDashboard"
 import ProductFeatureGroup from "@components/section/ProductFeatureGroup"
 import { Product } from "@interfaces/product"
+import useTrans from "lang/useTrans"
 
 const D4CTKL: VFC<{ product: Product, isRefurbished: boolean }> = ({ product, isRefurbished }) => {
+  const { $html, say } = useTrans()
   return (
     <>
       <ProductHero
-        heading="MINIMAL FOOTPRINT"
-        title="MAXIMUM POTENTIAL"
+        heading={say('model-4C-tenkeyless-professional_slider_1_title')}
+        title={say('model-4C-tenkeyless-professional_slider_1_subtitle')}
         isCherry={true}
         hasPrice={true}
         price={isRefurbished ? product.refurbished.price : product.price}
@@ -24,13 +26,11 @@ const D4CTKL: VFC<{ product: Product, isRefurbished: boolean }> = ({ product, is
       />
 
       <AppletDashboard
-        title="ULTRA-MODERN, COMPACT DESIGN"
+        title={say('slogan_model-4C-tenkeyless-professional_title_1')}
         description={`
-          The 4C TKL features PBT keycaps, Cherry MX Brown switches, and a 2-port USB hub in a sleek tenkeyless (TKL) format. It's made of the highest-quality materials and has a solid construction you can feel. The Das Keyboard 4C TKL, helps maximize productivity while optimizing your desktop footprint. Featuring a sleek black anodized aluminum top panel, accented by charcoal grey PBT keycaps, with black sublimation legends, this TKL mechanical keyboard will have everyone envious.
-          <br/><br/>
-          Charcoal PBT keycaps for maximum durability are paired with Cherry MX Brown switches, giving your new mechanical keyboard life up to 50 million keystrokes. You'll enjoy the tactile experience you love from a mechanical keyboard, with just enough sound to satisfy you - and not annoy your coworkers!
-          <br/><br/>
-          Built with the same best-in-class materials you've come to expect from Das Keyboard, our latest model in TKL keyboard format includes a solid construction meant to streamline your work environment while maximizing your potential for speed.
+          ${say('slogan_model-4C-tenkeyless-professional_content_1')}
+          ${say('slogan_model-4C-tenkeyless-professional_content_1.1')}
+          ${say('slogan_model-4C-tenkeyless-professional_content_1.2')}
         `}
         image={{
           src: "/images/product-images/product-details/4c-tkl/daskeyboard-4C-tenkeyless-professional-rear-view.png",
@@ -56,7 +56,7 @@ const D4CTKL: VFC<{ product: Product, isRefurbished: boolean }> = ({ product, is
         userManual="https://docs.google.com/document/d/1CU2jnB3jaOifmPNesTwSE6phJu_D2K2CmjeQ3uGmMB8/edit?usp=sharing"
       />
 
-      <NKeyRollover />
+      <NKeyRollover description={say('slogan_model-4C-tenkeyless-professional_content_7')} />
 
       <FootbarRuler />
 
@@ -66,7 +66,7 @@ const D4CTKL: VFC<{ product: Product, isRefurbished: boolean }> = ({ product, is
             type: 'content',
             heading: 'USB CONVENIENCE AT YOUR FINGERTIPS',
             title: 'TWO-PORT USB HUB',
-            description: 'The convenience to charge USB devices and simultaneously upload content through USB is now right at your fingertips.',
+            description: say('slogan_model-4C-tenkeyless-professional_content_9'),
             image: {
               position: 'left',
               src: '/images/product-images/product-details/4c-tkl/dk4c-tkl-usb-ports.jpg'
@@ -75,8 +75,8 @@ const D4CTKL: VFC<{ product: Product, isRefurbished: boolean }> = ({ product, is
           {
             type: 'content',
             heading: 'BRAND NEW',
-            title: 'DAS KEYBOARD PBT KEYCAPS IN CHARCOAL',
-            description: 'Charcoal grey PBT keycaps set against a black aluminum top panel give you a TKL mechanical keyboard with a polished and sophisticated appearance. This understated sleek design features the durability of PBT keycaps, allowing you to enjoy the fresh-out-of-the-box experience for years to come.',
+            title: say('slogan_model-4C-tenkeyless-professional_title_11'),
+            description: say('slogan_model-4C-tenkeyless-professional_content_11'),
             image: {
               position: 'right',
               src: '/images/product-images/product-details/4c-tkl/daskeyboard-4C-tenkeyless-professional-rear-view_png.jpg'
